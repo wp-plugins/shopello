e7c12093f21930b313f854b8aeb68cf6a96464e9
Shopello <Shopello@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-17 14:38:14 +0000

    New release 2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/shopello/trunk@1137226 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ src/classes/SWP/View.php
@@ -0,0 +1,44 @@
+<?php
+namespace SWP;
+
+use \Twig_Loader_Filesystem;
+use \Twig_Environment;
+use \Twig_Extension_Debug;
+use \Twig_SimpleFilter;
+
+class View
+{
+    /** @var Twig_Loader_Filesystem */
+    private $twigLoader;
+    /** @var Twig_Environment */
+    private $twigEnvironment;
+
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (empty(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct()
+    {
+        $this->twigLoader = new Twig_Loader_Filesystem(SHOPELLO_PLUGIN_DIR.'src/twig/');
+        $this->twigEnvironment = new Twig_Environment($this->twigLoader, array( 'debug' => true ));
+
+        $this->twigEnvironment->addExtension(new Twig_Extension_Debug());
+
+        // Add cast_to_array to cast objects to arrays
+        $this->twigEnvironment->addFilter(new Twig_SimpleFilter('cast_to_array', function ($stdClassObject) {
+            return (array) $stdClassObject;
+        }));
+    }
+
+    public function render($template, $data = array())
+    {
+        return $this->twigEnvironment->render($template.'.twig', $data);
+    }
+}
