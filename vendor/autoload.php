f6b4c7677fa5b32af0eef24d9b7d07473f9e9ac0
Shopello <Shopello@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-06 08:27:39 +0000

    Tagging version 2.0.5
    git-svn-id: https://plugins.svn.wordpress.org/shopello/tags/2.0.5@1154390 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitbf1fc02b4a9d71fd759b24b7c397a190::getLoader();
+return ComposerAutoloaderInita6bf7ef34a8ef4b4dc199faa9ebf8412::getLoader();
